392bc4b9d3ed26b89e4bd105ee701eda899d003d
IAAcastillo <izumia420@gmail.com>
2026-06-08 14:52:48 +0000

    completed data shapes project!

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -66,10 +66,18 @@ function handleStatic(data){
 }
 
   // TODO 4-a: add a function that handles the good display type
-  
+  function handleGood(color, shape, repeat){
+    setBackgroundWithSimple(color, shape, repeat);
+    animationDetails.displayType = 2;
+  }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+ 
+  function handleBad(data, repeat){
+    repeat = repeat += 1;
+    setBackgroundWithMixed(data, repeat);
+    animationDetails.displayType = 3;
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -82,12 +90,15 @@ function handleStatic(data){
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes[currentIndex];
+    handleGood(currentShape.color, currentShape.shape, currentShape.repeat)
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[currentIndex];
+    var repeat = currentShape.repeat;
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
